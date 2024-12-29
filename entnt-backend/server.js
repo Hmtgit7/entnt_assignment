642593610a984deea61da843fc448ce54e0fb211
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-12-29 07:52:40 +0530

    implementation of analytics and charts

--- entnt-backend/server.js
+++ entnt-backend/server.js
@@ -7,12 +7,13 @@ const CommRoutes = require('./routes/communicationMethodRoutes');
 const AuthRoutes = require('./routes/authRoutes');
 const NotificationRoutes = require('./routes/notificationRoutes');
 const CommunicationRoutes = require('./routes/communicationRoutes');
+const analyticsRoutes = require('./routes/analytics');
 const dotenv = require('dotenv');
 const User = require("./models/User");
 
 const app = express();
 app.use(cors({
-  origin: process.env.REACT_APP_FRONTEND_URL || 'http://localhost:5173'|| 'http://localhost:3000',
+  origin: process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 
@@ -54,6 +55,7 @@ app.use("/api/communications", CommRoutes);
 app.use("/api/notifications", NotificationRoutes);
 app.use("/api/communications-user", CommunicationRoutes);
 app.use("/api", AuthRoutes);
+app.use('/api/analytics', analyticsRoutes);
 
 // Start Server
 const PORT = 5000;
